a3cd23fe584e4de5326ea155cf79d360ce574059
Paweł Buczek <151944237+Buczy8@users.noreply.github.com>
2025-05-31 12:28:57 +0200

    feat: add game statistics

---
+++ src/main/java/org/example/StatisticsData.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class StatisticsData {
+    public int gamesPlayed;
+    public int easyWins;
+    public int easyLosses;
+    public int mediumWins;
+    public int mediumLosses;
+    public int hardWins;
+    public int hardLosses;
+}
